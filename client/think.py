b70fc1675a614face1c8a782b7714a8c1e50b927
wuhao_win <wu.hao.cz.21@gmail.com>
2016-10-18 23:21:24 +0800

    Change the data request function flow.

--- client/think.py
+++ client/think.py
@@ -1,5 +1,4 @@
 from socket import *
-from time import ctime
 import time
 import psycopg2
 import psycopg2.extras
@@ -15,20 +14,20 @@ TIMEOUT = 10.0
 def think_request(number_of_records, end_time):
 	ADDR = (HOST, PORT)
 	tcpCliSock = socket(AF_INET, SOCK_STREAM)
-	while True:
-		tcpCliSock.connect(ADDR)
-		tcpCliSock.settimeout(10.0)
-		msg = tcpCliSock.recv(BUFSIZ).decode('utf8')
-		if msg=='who':
-			tcpCliSock.send('algo'.encode("UTF-8"))
-			time.sleep(1)
-			request = 'request %s %s'%(number_of_records, end_time)#end_time="YYYY-mm-dd|HH:MM:SS:ffffff"
+	tcpCliSock.connect(ADDR)
+	tcpCliSock.settimeout(TIMEOUT)
+	msg = tcpCliSock.recv(BUFSIZ).decode('utf8')
+	if msg=='who':
+		tcpCliSock.send('algo'.encode("UTF-8"))
+		time.sleep(1)
+		request = 'request %d %s'%(number_of_records, end_time)#end_time="YYYY-mm-dd|HH:MM:SS:ffffff"
+		tcpCliSock.settimeout(TIMEOUT)
+		for _ in range(int(number_of_records)):
 			data = tcpCliSock.recv(BUFSIZ).decode('utf8')
-			break;
 		else:
-			continue;
-		tcpCliSock.close()
-	print(data)
+			print('Connected to wrong server.')
+	tcpCliSock.close()
+	#print(data)
 
 if __name__ == "__main__":
-        think_request('100', 'YYYY-mm-dd|HH:MM:SS:ffffff')
+        think_request(10, 'YYYY-mm-dd|HH:MM:SS:ffffff')
