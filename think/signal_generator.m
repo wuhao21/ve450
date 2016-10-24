6b76f5c19e4a504cb17ec97b6b2aedb5a03807ad
wuhao_win <wu.hao.cz.21@gmail.com>
2016-10-24 13:56:24 +0800

    from easy push

--- think/signal_generator.m
+++ think/signal_generator.m
@@ -3,7 +3,7 @@ x = [-5:0.1:5];
 fout = fopen('raw_data.txt','w');
 for i = 1:num_for_each
 	i
-	%SIN 1
+	%SIN 0
 	A = rand()+1;
 	b = rand()*pi;
 	c = rand();
@@ -11,16 +11,16 @@ for i = 1:num_for_each
 	for j = 1:size(y,2)
 		fprintf(fout,'%4.5f\t',y(j));
 	end
-	fprintf(fout, '%d\n', 1);
-	%LINEAR 2
+	fprintf(fout, '%d\n', 0);
+	%LINEAR 1
 	A = rand()+1;
 	b = rand();
 	y = A*x+b;
 	for j = 1:size(y,2)
 		fprintf(fout,'%4.5f\t',y(j));
 	end
-	fprintf(fout, '%d\n', 2);
-	%QUADRATIC 3
+	fprintf(fout, '%d\n', 1);
+	%QUADRATIC 2
 	A = rand()+1;
 	b = rand();
 	c = rand();
@@ -28,6 +28,6 @@ for i = 1:num_for_each
 	for j = 1:size(y,2)
 		fprintf(fout,'%4.5f\t',y(j));
 	end
-	fprintf(fout, '%d\n', 3);
+	fprintf(fout, '%d\n', 2);
 end
 fclose(fout);
